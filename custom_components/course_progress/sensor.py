72b3ecf1bc41e1250db28e33cc126c38c35929fa
Jordan H <jordan@hrvy.uk>
2023-12-10 22:06:07 +0000

    (sensors) built and working for course tracking

--- custom_components/course_progress/sensor.py
+++ custom_components/course_progress/sensor.py
@@ -1,8 +1,11 @@
 """Sensor platform for integration_blueprint."""
 from __future__ import annotations
+from collections.abc import Mapping
+from typing import Any
 
 from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import UndefinedType
 
 from pycourseprogress.member import Member
 from pycourseprogress.classes import Class
@@ -46,13 +49,54 @@ class CourseProgressSensor(CourseProgressEntity, SensorEntity):
     ) -> None:
         """Initialize the sensor class."""
         super().__init__(coordinator, member, f"{entity_description.key}_{course.class_id}")
-        self._course = course
+        self._course_id = course.class_id
         self.entity_description = entity_description
-        if self.entity_description.key == "course_completion":
-            self.entity_description.name = f"{course.class_name} Progress"
 
     @property
-    def native_value(self) -> int:
+    def name(self) -> str | None:
+        """Return the name of the entity."""
+        if self.entity_description.key == "course_completion":
+            return f"{self._get_course.class_name} {self.entity_description.name}"
+
+        return super().name
+
+    @property
+    def _get_course(self) -> Class:
+        """Return the course for this entity."""
+        return self._member.get_class(self._course_id)
+
+    @property
+    def native_value(self) -> float:
         """Return the native value of the sensor."""
         if self.entity_description.key == "course_completion":
-            return self._course.progress
+            return self._calculate_course_progress
+
+    @property
+    def _calculate_course_progress(self) -> float:
+        """Calculate the course progress from the compentencies."""
+        comp_raw = self._get_course.competencies["scheme"]["tree"]["children"][0]["children"][0]
+        completed: dict = self._get_course.competencies["member"]["assessments"]["complete"]
+        completed.pop("/", None)
+        completed.pop(comp_raw["id"], None)
+        return ((len(completed))/len(comp_raw["children"])) * 100
+
+    def _convert_competencies(self) -> list:
+        """Convert competencies into a list of usable data for extra state attributes."""
+        competencies = []
+        comp_raw = self._get_course.competencies["scheme"]["tree"]["children"][0]["children"][0]
+        completed: dict = self._get_course.competencies["member"]["assessments"]["complete"]
+        for c in comp_raw["children"]:
+            title = c["content"][0]["value"]
+            id = c["id"]
+            competencies.append({
+                "title": title,
+                "completed": completed.get(id, False)
+            })
+
+        return competencies
+
+
+    @property
+    def extra_state_attributes(self) -> Mapping[str, Any] | None:
+        """Return a list of extra state attributes."""
+        return {"competencies": self._convert_competencies()}
